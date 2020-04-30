ef35d8a51f663b715b513fc6d7e59132d4bb237e
Konrad <konradszolc@gmail.com>
2020-04-30 14:24:07 +0200

    Added comparason of abs value and determinant of matrix

--- Zadanie4-1/src/main.cpp
+++ Zadanie4-1/src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include "sysOfLinEq.hh"
 #include "complex.hh"
+#include "matrix.hh"
 
 #define SIZE 5
 
@@ -30,9 +31,24 @@ int main(int argc, char** argv){
     
     }else{ // there is invalid number of arguments
         if(argc == 1){
+            
+            Matrix<double,2> macierz;
+            Complex zesp;
+
+            cin >> macierz;
+            cin >> zesp;
+
+            cout <<macierz;
+            cout <<endl; 
+            cout << zesp;
+
+            cout << endl<< "Abs of Complex: " <<zesp.absv() <<endl <<"Determinant: "<< macierz.determinant();
+
+            cout << endl <<endl;
+            /*
             cout << "There are to few arguments!" <<endl;
             cout << "Usage: "<<argv[0]<< " <r/c> (real/complex numbers)"<<endl;
-            return 1;
+            return 1; */
         }else{
             cout << "There are to much arguments!" << endl;
             cout << "Usage: ./"<<argv[0]<< " <r/c> (real/complex numbers)"<<endl;
